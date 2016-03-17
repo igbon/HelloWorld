dfd3d2c01f9c160458aa0645e6172d19ca10a5c8
Michael <Michael>
2016-03-18 02:00:53 +1100

    Initial commit

---
+++ app/src/androidTest/java/org/episol/mick/helloworld/ApplicationTest.java
@@ -0,0 +1,15 @@
+package org.episol.mick.helloworld;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application>
+{
+    public ApplicationTest()
+    {
+        super(Application.class);
+    }
+}
